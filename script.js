1c6a858a3b96a795fcf673e6d6e3f2622187e709
Simon Mwangi <simonmwangi749@gmail.com>
2025-08-25 14:29:04 +0300

    .....

--- script.js
+++ script.js
@@ -1,3 +1,36 @@
+function loading() {
+    var tl = gsap.timeline()
+
+
+    tl.to("#yellow1", {
+        top: "-100%",
+        delay: 0.5,
+        duration: 0.7,
+        ease: "expo.out"
+    })
+
+    tl.from("#yellow2", {
+        top: "100%",
+        duration: 0.7,
+        ease: "expo.out",
+        delay: 0.6
+    }, "anim")
+    tl.to("#loader h1", {
+        delay: 0.6,
+        duration: 0.7,
+        color: "black"
+    }, "anim")
+    tl.to("#loader", {
+        opacity: 0,
+    })
+    tl.to("#loader", {
+        display: "none",
+    })
+}
+
+loading();
+
+
 const scroll = new LocomotiveScroll({
     el: document.querySelector('#main'),
     smooth: true
