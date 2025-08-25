1e6c2d661c8a4a49b18be226ebf9c2e322f1e5af
Simon Mwangi <simonmwangi749@gmail.com>
2025-08-25 16:48:11 +0300

    ....

--- script.js
+++ script.js
@@ -35,3 +35,13 @@ const scroll = new LocomotiveScroll({
     el: document.querySelector('#main'),
     smooth: true
 });
+
+
+var elems = document.querySelectorAll(".elem")
+var page2 = document.querySelector("#page2")
+elems.forEach(function (ele) {
+    ele.addEventListener("mouseenter", function () {
+        var bgimg = ele.getAttribute("data-img")
+        page2.style.backgroundImage = `url(${bgimg})`
+    })
+})
