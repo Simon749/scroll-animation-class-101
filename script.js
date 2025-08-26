7c5798597d8ef82a413a11c50e4dc0fed8d4f5a8
Simon Mwangi <simonmwangi749@gmail.com>
2025-08-26 15:44:38 +0300

    ...

--- script.js
+++ script.js
@@ -31,21 +31,25 @@ function loading() {
 loading();
 
 
-const scroll = new LocomotiveScroll({
-    el: document.querySelector('#main'),
-    smooth: true
-});
+function loco() {
+    const scroll = new LocomotiveScroll({
+        el: document.querySelector('#main'),
+        smooth: true
+    });
 
 
-document.querySelector("#footer h2").addEventListener("click", () => {
-    scroll.scrollTo(0)
-})
-
-var elems = document.querySelectorAll(".elem")
-var page2 = document.querySelector("#page2")
-elems.forEach(function (ele) {
-    ele.addEventListener("mouseenter", function () {
-        var bgimg = ele.getAttribute("data-img")
-        page2.style.backgroundImage = `url(${bgimg})`
+    document.querySelector("#footer h2").addEventListener("click", () => {
+        scroll.scrollTo(0)
     })
-})
+
+    var elems = document.querySelectorAll(".elem")
+    var page2 = document.querySelector("#page2")
+    elems.forEach(function (ele) {
+        ele.addEventListener("mouseenter", function () {
+            var bgimg = ele.getAttribute("data-img")
+            page2.style.backgroundImage = `url(${bgimg})`
+        })
+    })
+}
+
+loco();
