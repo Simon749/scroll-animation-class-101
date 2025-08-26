f15f5e3b860fc12c736b66d2f618478f93a3f163
Simon Mwangi <simonmwangi749@gmail.com>
2025-08-26 13:17:13 +0300

    ...

--- script.js
+++ script.js
@@ -37,6 +37,10 @@ const scroll = new LocomotiveScroll({
 });
 
 
+document.querySelector("#footer h2").addEventListener("click", () => {
+    scroll.scrollTo(0)
+})
+
 var elems = document.querySelectorAll(".elem")
 var page2 = document.querySelector("#page2")
 elems.forEach(function (ele) {
